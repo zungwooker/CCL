abaf883f0411bcfdafb3187f50b43fd74ebfedb2
zungwooker <zungwooker@gmail.com>
2021-05-24 13:13:42 +0900

    Initial Commit

---
+++ CCL/CCLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCLApp.swift
+//  CCL
+//
+//  Created by blair on 2021/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CCLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCL
+//
+//  Created by blair on 2021/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
